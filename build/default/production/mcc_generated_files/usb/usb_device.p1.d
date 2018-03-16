d6ea8bd39f844abb1faebdf95783c96b900a9c72
LarsErik <LarsErik@DESKTOP-O2VKKO7>
2018-03-16 13:15:17 +0100

    a

---
+++ build/default/production/mcc_generated_files/usb/usb_device.p1.d
@@ -0,0 +1,13 @@
+ build/default/production/mcc_generated_files/usb/usb_device.d  \
+ build/default/production/mcc_generated_files/usb/usb_device.p1:  \
+ mcc_generated_files/usb/usb_device.c  \
+mcc_generated_files/usb/usb_common.h  \
+mcc_generated_files/usb/usb_hal_pic16f1.h  \
+mcc_generated_files/usb/usb_ch9.h  \
+mcc_generated_files/usb/usb_device_local.h  \
+mcc_generated_files/usb/usb_device.h  \
+mcc_generated_files/usb/usb_hal.h  \
+mcc_generated_files/usb/usb_device_config.h  \
+mcc_generated_files/usb/usb_device_cdc.h  \
+mcc_generated_files/usb/usb.h  \
+mcc_generated_files/usb/fixed_address_memory.h 
